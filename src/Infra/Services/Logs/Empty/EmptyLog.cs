23b69b3415a6643d5dd9554d7008dac4b4af7139
Luís Fernando O. Caldeira <luisfcaldeira@gmail.com>
2023-03-28 01:44:38 -0300

    Conversor DTO to Neural Network: layer collection made.

---
+++ src/Infra/Services/Logs/Empty/EmptyLog.cs
@@ -0,0 +1,11 @@
+﻿using MyNeuralNetwork.Domain.Interfaces.Services.Logs;
+
+namespace Core.Infra.Services.Logs.Empty
+{
+    public class EmptyLog : ITraceLog
+    {
+        public void Log(object message)
+        {
+        }
+    }
+}
---
+++ src/Infra/Services/Logs/OutputWindow/DebuggerLog.cs
@@ -0,0 +1,13 @@
+﻿using MyNeuralNetwork.Domain.Interfaces.Services.Logs;
+using System.Diagnostics;
+
+namespace Core.Infra.Services.Logs.OutputWindow
+{
+    public class DebuggerLog : ITraceLog
+    {
+        public void Log(object message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
