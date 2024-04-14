b3b1754e30ef732c30ab95316dd7d261218a4ad0
Luís Fernando Caldeira <luisfcaldeira@hotmail.com.br>
2024-04-13 22:50:27 -0300

    Created read.me in Portuguese.

--- src/ConsoleApp.Tests/Program.cs
+++ src/ConsoleApp.Tests/Program.cs
@@ -23,7 +23,7 @@ namespace ConsoleApp.Example
 
             var nngen = new NNGenerator(ngen, new LayersLinker());
 
-            var neuralNetwork = nngen.Generate<SynapseManager, Tanh>(new int[] { 2, 18, 1 });
+            var neuralNetwork = nngen.Generate<SynapseManager, Relu>(new int[] { 2, 100, 1 });
 
             var dataManager = GenDataForAndGate();
 
